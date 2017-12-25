5172f2eb2f8046f4f6d54e7df7cd2f40357f9698
Mathias Ertl <mati@er.tl>
2017-12-25 13:27:48 +0100

    cleanup, ua-detection is done server-side

--- xmpp_software_overview/static/xmpp_software_overview/xmpp_software_overview.js
+++ xmpp_software_overview/static/xmpp_software_overview/xmpp_software_overview.js
@@ -1,51 +1,5 @@
-/**
- * Bare function to detect the operating system platform.
- *
- * Will return either 'linux', 'android', 'win', 'osx', 'ios' or an empty string, in which
- * case detection failed.
- */
-function xmpp_software_overview_detect_platform() {
-    /** 
-     * According to https://github.com/bestiejs/platform.js/blob/1.3.4/platform.js#L1141:
-     *
-     * Common values include:
-     * "Windows", "Windows Server 2008 R2 / 7", "Windows Server 2008 / Vista",
-     * "Windows XP", "OS X", "Ubuntu", "Debian", "Fedora", "Red Hat", "SuSE",
-     * "Android", "iOS" and "Windows Phone"
-     */
-
-    let family = platform.os.family.toLowerCase();
-    console.log('family "' + family + '" detected');
-
-    if (/^android/i.test(family)) {
-        return 'android';
-    } else if (family === 'ios') {
-        return 'ios';
-    } else if (/^(linux|ubuntu|debian|fedora|red hat|suse)/i.test(family)) {
-        return 'linux';
-    } else if (/^windows/i.test(family)) {
-        return 'win';
-    } else if (/^os\s*x/i.test(family)) {
-        return 'osx';
-    }
-    return 'any';
-};
-
 function xmpp_software_overview_show_os_specific(platform) {
-    if (typeof platform === 'undefined') {
-        // first get any os=value query parameter (...?os=linux)
-        var url = new URL(document.location);
-        var platform = url.searchParams.get('os');
-
-        // If nothing was requested via query string, try to detect platform
-        if (! platform) {
-            var platform = xmpp_software_overview_detect_platform();
-        }
-
-        if (typeof platform !== 'undefined') {
-            $('select#os-selector').val(platform);
-        }
-    }   
+    /* TODO: apply_attrs() is only used for columns, can we get that dynamically?  */
 
     if (platform == 'any' || typeof platform == 'undefined') {
         $('[class^="os-"], [class*=" os-*]').show();
@@ -65,8 +19,6 @@ function xmpp_software_overview_show_os_specific(platform) {
 };
 
 $(document).ready(function() {
-    xmpp_software_overview_show_os_specific();
-
     $('select#os-selector').change(function(e) {
         var selected = $(e.target).val();
         xmpp_software_overview_show_os_specific(selected);
