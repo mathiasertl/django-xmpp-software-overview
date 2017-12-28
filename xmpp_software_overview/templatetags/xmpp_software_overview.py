3cc830e467b5ca76b792ab7560ed333e87f7eda0
Mathias Ertl <mati@er.tl>
2017-12-28 14:33:37 +0100

    code quality

--- xmpp_software_overview/templatetags/xmpp_software_overview.py
+++ xmpp_software_overview/templatetags/xmpp_software_overview.py
@@ -16,8 +16,8 @@
 
 from django import template
 from django.forms.utils import flatatt
-from django.utils.safestring import mark_safe
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 register = template.Library()
 
